04e68a202e99ca9f6ef8c77e9f37dabbe938367c
Mosope-Adeyemi-dev <78312219+Mosope-Adeyemi-dev@users.noreply.github.com>
2023-03-06 23:55:54 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const { readdirSync } = require("fs");
+const cors = require("cors")
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 // all routes are immediately loaded when created
 readdirSync("./routes").map(async (routeFile) => {
